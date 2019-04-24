0953149407f713e5e28b3c5b18cea0fd8290ab32
Naman Avasthi <naman.avasthi@gmail.com>
2019-04-23 22:31:44 -0700

    added comments for the single sine wave code

--- main.js
+++ main.js
@@ -12,26 +12,34 @@ function draw() {
 
   let radius = 100;
 
+  // create the base circle(s)
   stroke(255);
   noFill();
   ellipse(0, 0, radius * 2);
 
+  // plot the points on circle(s)
   let x = radius * cos(-time);
   let y = radius * sin(-time);
 
+  // save y co-ordinate values in an array
   wave.unshift(y);
 
+  // plot the radius
   line(0, 0, x, y);
 
+  // plot the point(s) on the circle(s)
   fill(255, 0, 0);
   stroke(255, 0, 0);
   ellipse(x, y, 8);
 
+  // clear the stroke color and traslate to plot the wave
   stroke(255);
   translate(200, 0);
 
+  // create the line from the wave to the circle(s)
   line(x - 200, y, 0, wave[0]);
 
+  // create the sine shape
   beginShape();
   noFill();
   for (let i = 0; i < wave.length; i++) {
