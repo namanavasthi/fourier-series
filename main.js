9e77c5222619477d050697d9032de55844b80ce5
Naman Avasthi <naman.avasthi@gmail.com>
2019-04-23 22:15:10 -0700

    initial styles and canvas structure

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log("Hello World!");
+let time = 0;
+
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(0);
+  translate(200, 200);
+
+  let radius = 100;
+
+  stroke(255);
+  noFill();
+  ellipse(0, 0, radius * 2);
+}
